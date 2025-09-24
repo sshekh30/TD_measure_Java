c3fd5628298920cf6584de7468588abcf29c678d
Kushagra1480 <kushagrakartik1480@gmail.com>
2025-09-24 13:05:23 -0700

    added sys, trainee & team entropy

--- Task-30/JS_to_java/java/Entropy/src/Main.java
+++ Task-30/JS_to_java/java/Entropy/src/Main.java
@@ -1,59 +1,56 @@
-package src;
-
-import dao.*;
 import config.*;
-import parser.*;
+import dao.*;
 import entropy.*;
-import visualizer.*;
-
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
+import parser.*;
+import visualizer.*;
 
 public class Main {
 
     public static void dumpToCSV(double[][] data, String fileName) {
-
         try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
-        
-        int totalTime = data[0].length;
+            int totalTime = data[0].length;
 
-        writer.print("Time");
-        for (int t = 0; t < totalTime; t++) {
-            writer.print("," + t);
-        }
-        writer.println();
-
-        for (int layer = 0; layer < data.length; layer++) {
-            writer.print("Layer " + layer);
+            writer.print("Time");
             for (int t = 0; t < totalTime; t++) {
-            writer.print("," + data[layer][t]);
+                writer.print("," + t);
             }
             writer.println();
-        }
 
-        System.out.println("Data exported to: " + fileName);
-        } catch(IOException e) {
-        System.out.println("Error writing to file: " + e.getMessage());
+            for (int layer = 0; layer < data.length; layer++) {
+                writer.print("Layer " + layer);
+                for (int t = 0; t < totalTime; t++) {
+                    writer.print("," + data[layer][t]);
+                }
+                writer.println();
+            }
+
+            System.out.println("Data exported to: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
         }
     }
 
-    public static void dumpToFile(List<String> data, String fileName) throws IOException {
-       try (FileWriter writer = new FileWriter(fileName)) {
-           writer.write("[\n");
-           for (int i = 0; i < data.size(); i++) {
-               writer.write("  " + data.get(i));
-               if (i < data.size() - 1) {
-                   writer.write(",");
-               }
-               writer.write("\n");
-           }
-           writer.write("]\n");
-           System.out.println("Data dumped to: " + fileName);
-       }
+    public static void dumpToFile(List<String> data, String fileName)
+        throws IOException {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write("[\n");
+            for (int i = 0; i < data.size(); i++) {
+                writer.write("  " + data.get(i));
+                if (i < data.size() - 1) {
+                    writer.write(",");
+                }
+                writer.write("\n");
+            }
+            writer.write("]\n");
+            System.out.println("Data dumped to: " + fileName);
+        }
     }
 
-    private static String[][][] convertToArray(List<List<List<String>>> listData) {
+    private static String[][][] convertToArray(
+        List<List<List<String>>> listData
+    ) {
         String[][][] arrayData = new String[listData.size()][][];
         for (int i = 0; i < listData.size(); i++) {
             List<List<String>> timePoint = listData.get(i);
@@ -67,24 +64,85 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        ConfigManager config = new ConfigManager("resources/runtime.properties");
+        ConfigManager config = new ConfigManager(
+            "resources/runtime.properties"
+        );
         DataSourceDAO dataSource = DataSourceFactory.createDataSource(config);
-        DataParser parser = ParserFactory.createParser(config.getDataSourceType());
+        DataParser parser = ParserFactory.createParser(
+            config.getDataSourceType()
+        );
 
         List<String> rawData = dataSource.readData();
         List<List<List<String>>> layers = parser.parseToSTTCLayers(rawData);
-        String[][][] layerData = convertToArray(layers);  
-        
-        List<double[]> entropies = new ArrayList<>();
+        String[][][] layerData = convertToArray(layers);
+
+        List<double[]> allEntropies = new ArrayList<>();
+        List<String> entropyNames = new ArrayList<>();
+
+        String[] layerNames = {
+            "Communication",
+            "Visual",
+            "Casualty",
+            "Movement",
+        };
         for (int i = 0; i < 4; i++) {
-          AggregationStrategy strategy = new LayerAggregationStrategy(i);
-          double[] layerEntropy = GeneralizedEntropyCalculator.computeWindowedEntropy(layerData, 4, strategy);
-          entropies.add(layerEntropy);
-          System.out.println("Layer " + i + " entropy: " + Arrays.toString(layerEntropy));
+            AggregationStrategy strategy = new LayerAggregationStrategy(i);
+            double[] layerEntropy =
+                GeneralizedEntropyCalculator.computeWindowedEntropy(
+                    layerData,
+                    4,
+                    strategy
+                );
+            allEntropies.add(layerEntropy);
+            entropyNames.add(layerNames[i]);
         }
+
+        for (int traineeId = 0; traineeId < 3; traineeId++) {
+            AggregationStrategy strategy =
+                new IndividualEntityAggregationStrategy(
+                    new int[] { 0, 1, 3 },
+                    traineeId
+                );
+            double[] traineeEntropy =
+                GeneralizedEntropyCalculator.computeWindowedEntropy(
+                    layerData,
+                    4,
+                    strategy
+                );
+            allEntropies.add(traineeEntropy);
+            entropyNames.add("Trainee: " + (traineeId + 1));
+        }
+
+        AggregationStrategy systemStrategy =
+            new CombinedLayerAggregationStrategy(new int[] { 0, 1, 2, 3 });
+        double[] systemEntropy =
+            GeneralizedEntropyCalculator.computeWindowedEntropy(
+                layerData,
+                4,
+                systemStrategy
+            );
+        allEntropies.add(systemEntropy);
+        entropyNames.add("System");
+
+        AggregationStrategy teamStrategy = new CombinedLayerAggregationStrategy(
+            new int[] { 0, 3 }
+        );
+        double[] teamEntropy =
+            GeneralizedEntropyCalculator.computeWindowedEntropy(
+                layerData,
+                4,
+                teamStrategy
+            );
+        allEntropies.add(teamEntropy);
+        entropyNames.add("Team");
+
         EntropyVisualizer viz = new EntropyVisualizer();
-        viz.generateVisualization(entropies, "team_dynamics_entropy.html");
-          
+        viz.generateVisualization(
+            allEntropies,
+            entropyNames,
+            "team_dynamics_entropy.html"
+        );
+
         // System.out.println(layers);
 
         // dumpToFile(rawData, "mongodb_raw_data.json");
